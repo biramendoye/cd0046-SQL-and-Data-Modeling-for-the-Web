0b90718d15b1567b99dc13a84df4ecf7a8f40061
Birame Ndoye <106784249+biramendoye@users.noreply.github.com>
2022-08-07 14:06:44 +0000

    Complete delete venue endpoint

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,16 @@ window.parseISOString = function parseISOString(s) {
   var b = s.split(/\D+/);
   return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
 };
+
+const deleteVenueButton = document.getElementById("deleteVenue");
+deleteVenueButton.onclick = function (e) {
+  const venue_id = e.target.dataset.id;
+  fetch("/venues/" + venue_id, {
+    "method": "DELETE"
+  }).then(function(response) {
+    console.log(response);
+    window.location.href = '/';
+  }).catch(function(e){
+      console.log(e);
+    });
+};
